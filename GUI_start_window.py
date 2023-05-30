15e339a5e27319735fb59403efef7cc877adb7f3
Mathieu <mathieu.stawarz@epfl.ch>
2023-05-30 17:02:02 +0200

    GUI start window V2

--- GUI_start_window.py
+++ GUI_start_window.py
@@ -1,6 +1,6 @@
 # Farmbot project 2023, Sensors & UI, Mathieu Stawarz
 # GUI_start_windows.py
-# pyside6 GUI starting window display and prepare next windows
+# GUI starting window class
 
 # Pyside6 module :
 # pip3 install pyside6
@@ -19,13 +19,16 @@ from PySide6.QtCore import Qt, QTimer, QUrl
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from PySide6.QtGui import QPixmap
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput #music !!
+from PySide6.QtMultimediaWidgets import QVideoWidget #video widget
+
+##
+##
+## Modules Class imports from other files 
+##
+##
 
 
-##
-##
-## Charts display
-##
-##
+from GUI_menu_window import menuWindow #import menuwindow class
 
 
 
@@ -46,37 +49,54 @@ class Window(QMainWindow):
         self.setStyleSheet("background-color: rgb(25,25,25);")
         self.top = 125 # pixels # Where the window will be located 
         self.left = 100
-        self.width = 1200 #pixels # Size of the window 
-        self.height = 600
-
-        #Push Button startwindow
-        self.pushButton = QPushButton("Start", self)
-        self.pushButton.setStyleSheet("background-color: rgb(46, 139, 87); color: white; border: 2px solid white;") #Nice green colour
-        self.button_width = 200
-        self.button_height = 75
-        self.playerb = QMediaPlayer()
-        self.audio_outputb = QAudioOutput()
-
-        self.pushButton.clicked.connect(self.openWindow2)  # <===
-
-        #Start window songs players
-        self.player = QMediaPlayer()
-        self.audio_output = QAudioOutput()
+        self.width = 1250 #pixels # Size of the window 
+        self.height = 650
 
         self.startWindow() # call start window method
     
+    ##starting window setup
     # separate method that sets up the main interface of the window by creating and 
     # configuring widgets like labels and setting the window's title and geometry.
-
-    #starting window
     def startWindow(self):
-        #Plays start window song
+        #plays Startwindow song 
+        self.player = QMediaPlayer()
+        self.audio_output = QAudioOutput()
         self.player.setAudioOutput(self.audio_output)
         self.player.setSource(QUrl.fromLocalFile("GUI_medias/relaxing_song.mp3"))
         self.audio_output.setVolume(50)
         self.player.play()
 
-        #icon label1
+        ##QPushbutton widget
+        self.pushButton = QPushButton("Start", self)
+        self.pushButton.clicked.connect(self.open_menuWindow)  # <=== connect to open_menuWindow submethod
+        #Qbutton widget setup
+        self.pushButton.setStyleSheet("background-color: rgb(46, 139, 87); color: white; border: 2px solid white;") #Nice green colour
+        self.button_width = 200
+        self.button_height = 75
+        fontp = self.pushButton.font()
+        fontp.setPointSize(25)  # Set the font size to 16
+        fontp.setBold(True)
+        self.pushButton.setFont(fontp)
+        self.pushButton.setFixedSize(self.button_width, self.button_height)
+        self.pushButton.move( (self.width - self.button_width) / 2 , (self.height - self.button_height) / 2)
+        self.pushButton.setToolTip("<h3 style='background-color: beige; color: black; border: none'> Start the GUI - Data acquisition </h3>") # small pop-up text near the button
+
+        ##Video Widget
+        video_widget = QVideoWidget(self) 
+        self.playerv = QMediaPlayer()
+        self.playerv.mediaStatusChanged.connect(self.handle_media_status_changed) # <=== connect to handle_media_status_changed submethod, restart music !
+        #Video widget setup
+        video_widget_width = 400/1.25 #400pixel orig. size
+        video_widget_height = 300/1.25 #300pixels orig. size
+        video_widget.setFixedSize(video_widget_width, video_widget_height)
+        video_widget.move(self.width - video_widget_width - 50, self.height - video_widget_height - 50)
+        video_widget.setToolTip("<h3 style='background-color: beige; color: black; border: none'> Sensor Panel overview </h3>")
+        self.playerv.setVideoOutput(video_widget)
+        self.playerv.setSource(QUrl.fromLocalFile("GUI_medias/sensor_panel_video.mp4"))
+        self.playerv.play()
+
+
+        #icon label1 widget
         icon_label = QLabel(self)
         icon_top = 50 # pixels # Where the window will be located ?
         icon_left = 50
@@ -109,9 +129,9 @@ class Window(QMainWindow):
 
         #icon label3
         icon_label3 = QLabel(self)
-        icon_label3_width = 1340/6 #1340pixel orig. size
-        icon_label3_height = 1282/6 #1282pixels orig. size
-        icon_top3 = self.height - icon_label3_height  - 50  # pixels # Where the window will be located ?
+        icon_label3_width = 1340/7 #1340pixel orig. size
+        icon_label3_height = 1282/7 #1282pixels orig. size
+        icon_top3 =  50  # pixels # Where the window will be located ?
         icon_left3 = self.width - icon_label3_width - 50 
         icon_label3.setFixedSize(icon_label3_width, icon_label3_height)
         icon_label3.setAlignment(Qt.AlignCenter)
@@ -126,8 +146,8 @@ class Window(QMainWindow):
         font.setPointSize(32)  # Set the font size 
         font.setBold(True)
         label.setFont(font)
-        label_width = 1000
-        label_height = 100
+        label_width = 575
+        label_height = 50
         label.setFixedSize(label_width, label_height)
         label.setAlignment(Qt.AlignCenter)
         label.move((self.width - label_width) / 2, (self.height - label_height) / 2 - 2 * self.button_height )
@@ -162,69 +182,48 @@ class Window(QMainWindow):
         label3.setToolTip("<h3 style='background-color: beige; color: black; border: none'> The motivated students who have worked on this project</h3>")
         #icon_label3.setStyleSheet("QToolTip { color: black; background-color: beige }")
 
-        #Qbutton style
-        fontp = self.pushButton.font()
-        fontp.setPointSize(25)  # Set the font size to 16
-        fontp.setBold(True)
-        self.pushButton.setFont(fontp)
-        self.pushButton.setFixedSize(self.button_width, self.button_height)
-        self.pushButton.move( (self.width - self.button_width) / 2 , (self.height - self.button_height) / 2)
-        self.pushButton.setToolTip("<h3 style='background-color: beige; color: black; border: none'> Start the GUI - Data acquisition </h3>") # small pop-up text near the button
-
         #window style and display
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
         self.show()
+    
+    #Restart the video and plays it again
+    def handle_media_status_changed(self, media_status):
+        if media_status == QMediaPlayer.EndOfMedia:
+            self.playerv.setPosition(0)  # Restart the video from the beginning
+            self.playerv.play()
 
     # Opens 2nd window with delay and plays a nice launch music !
-    def openWindow2(self): 
+    def open_menuWindow(self): 
         self.player.stop() #stops start music
-        self.pushButton.setEnabled(False)  # Disable the button temporarily
-        self.pushButton.setText("Launching ...")  # Set the button text clicks
         #Plays launch music
+        self.playerb = QMediaPlayer() #music button
+        self.audio_outputb = QAudioOutput()
         self.playerb.setAudioOutput(self.audio_outputb)
         self.playerb.setSource(QUrl.fromLocalFile("GUI_medias/success.mp3"))
         self.audio_outputb.setVolume(50)
         self.playerb.play()
+        #Push button update
+        self.pushButton.setEnabled(False)  # Disable the button temporarily
+        self.pushButton.setText("Launching ...")  # Set the button text clicks
         #Timer
-        QTimer.singleShot(3000, self.showWindow2)  # Start a timer for delay (ms)
+        QTimer.singleShot(3000, self.show_menuWindow)  # Start a timer for delay (ms)
 
     #Show window and close main window
-    def showWindow2(self):   
+    def show_menuWindow(self):   
         self.playerb.stop()                                          # <===
-        self.w = Window2() #Creates a new instance w
+        self.w = menuWindow() #Creates a new instance w
         self.w.show() #Show the new window 2
         self.close() #closes the main_window
 
 
+if __name__ == "__main__": # allows the code inside the block to run only if the script is executed directly, and not when it is imported as a module.
+    #You need one (and only one) QApplication instance per application. Pass in sys.argv to allow command line arguments for your app.
+    app = QApplication(sys.argv)
 
-## Window2
+    # Set the style sheet for all tooltips
+    window = Window()
 
-class Window2(QMainWindow):                           # <===
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Sensors Data")
-
-        # Specifies the distance from the top/bottom edge of the screen to the top/bottom edge of the window
-        # Where the window will be located ?
-        self.top = 100 # pixels
-        self.left = 100
-        # Specifies the distance from the top/bottom edge of the screen to the top/bottom edge of the window
-        # Size of the window ?
-        self.width = 2000 #pixels
-        self.height = 1500
-
-        self.setGeometry(self.top, self.left, self.width, self.height)
-
-
-#You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-app = QApplication(sys.argv)
-
-# Set the style sheet for all tooltips
-window = Window()
-
-# Start the event loop.
-app.exec()
-
-# Your application won't reach here until you exit and the event loop has stopped.
+    # Start the event loop.
+    app.exec()
+    # Your application won't reach here until you exit and the event loop has stopped.
